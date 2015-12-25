351f1a1e86df184dbbbfc91da0ea247834b1e93e
Steve K <steve@actor.im>
2015-12-25 15:00:02 +0300

    Release v1.0.70

--- actor.js
+++ actor.js
@@ -249,36 +249,36 @@ function actor(){
   processMetas();
   try {
     var strongName;
-    unflattenKeylistIntoAnswers(['ar', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B');
-    unflattenKeylistIntoAnswers(['ar', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':1');
-    unflattenKeylistIntoAnswers(['default', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':10');
-    unflattenKeylistIntoAnswers(['default', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':11');
-    unflattenKeylistIntoAnswers(['default', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':12');
-    unflattenKeylistIntoAnswers(['default', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':13');
-    unflattenKeylistIntoAnswers(['default', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':14');
-    unflattenKeylistIntoAnswers(['en', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':15');
-    unflattenKeylistIntoAnswers(['en', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':16');
-    unflattenKeylistIntoAnswers(['en', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':17');
-    unflattenKeylistIntoAnswers(['en', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':18');
-    unflattenKeylistIntoAnswers(['en', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':19');
-    unflattenKeylistIntoAnswers(['ar', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':2');
-    unflattenKeylistIntoAnswers(['pt', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':20');
-    unflattenKeylistIntoAnswers(['pt', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':21');
-    unflattenKeylistIntoAnswers(['pt', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':22');
-    unflattenKeylistIntoAnswers(['pt', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':23');
-    unflattenKeylistIntoAnswers(['pt', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':24');
-    unflattenKeylistIntoAnswers(['ru', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':25');
-    unflattenKeylistIntoAnswers(['ru', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':26');
-    unflattenKeylistIntoAnswers(['ru', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':27');
-    unflattenKeylistIntoAnswers(['ru', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':28');
-    unflattenKeylistIntoAnswers(['ru', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':29');
-    unflattenKeylistIntoAnswers(['ar', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':3');
-    unflattenKeylistIntoAnswers(['ar', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':4');
-    unflattenKeylistIntoAnswers(['cn', 'gecko1_8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':5');
-    unflattenKeylistIntoAnswers(['cn', 'ie10'], '1E2CB31439A7C1BCE101B96751551E2B' + ':6');
-    unflattenKeylistIntoAnswers(['cn', 'ie8'], '1E2CB31439A7C1BCE101B96751551E2B' + ':7');
-    unflattenKeylistIntoAnswers(['cn', 'ie9'], '1E2CB31439A7C1BCE101B96751551E2B' + ':8');
-    unflattenKeylistIntoAnswers(['cn', 'safari'], '1E2CB31439A7C1BCE101B96751551E2B' + ':9');
+    unflattenKeylistIntoAnswers(['ar', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1');
+    unflattenKeylistIntoAnswers(['ar', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':1');
+    unflattenKeylistIntoAnswers(['default', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':10');
+    unflattenKeylistIntoAnswers(['default', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':11');
+    unflattenKeylistIntoAnswers(['default', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':12');
+    unflattenKeylistIntoAnswers(['default', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':13');
+    unflattenKeylistIntoAnswers(['default', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':14');
+    unflattenKeylistIntoAnswers(['en', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':15');
+    unflattenKeylistIntoAnswers(['en', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':16');
+    unflattenKeylistIntoAnswers(['en', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':17');
+    unflattenKeylistIntoAnswers(['en', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':18');
+    unflattenKeylistIntoAnswers(['en', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':19');
+    unflattenKeylistIntoAnswers(['ar', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':2');
+    unflattenKeylistIntoAnswers(['pt', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':20');
+    unflattenKeylistIntoAnswers(['pt', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':21');
+    unflattenKeylistIntoAnswers(['pt', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':22');
+    unflattenKeylistIntoAnswers(['pt', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':23');
+    unflattenKeylistIntoAnswers(['pt', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':24');
+    unflattenKeylistIntoAnswers(['ru', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':25');
+    unflattenKeylistIntoAnswers(['ru', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':26');
+    unflattenKeylistIntoAnswers(['ru', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':27');
+    unflattenKeylistIntoAnswers(['ru', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':28');
+    unflattenKeylistIntoAnswers(['ru', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':29');
+    unflattenKeylistIntoAnswers(['ar', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':3');
+    unflattenKeylistIntoAnswers(['ar', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':4');
+    unflattenKeylistIntoAnswers(['cn', 'gecko1_8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':5');
+    unflattenKeylistIntoAnswers(['cn', 'ie10'], '0C13D2C542A46C8ABA264AD331488ED1' + ':6');
+    unflattenKeylistIntoAnswers(['cn', 'ie8'], '0C13D2C542A46C8ABA264AD331488ED1' + ':7');
+    unflattenKeylistIntoAnswers(['cn', 'ie9'], '0C13D2C542A46C8ABA264AD331488ED1' + ':8');
+    unflattenKeylistIntoAnswers(['cn', 'safari'], '0C13D2C542A46C8ABA264AD331488ED1' + ':9');
     strongName = answers[computePropValue('locale')][computePropValue('user.agent')];
     var idx = strongName.indexOf(':');
     if (idx != -1) {
@@ -317,7 +317,7 @@ function actor(){
 }
 
 actor();
-(function () {var $gwt_version = "2.7.0";var $wnd = window;var $doc = $wnd.document;var $moduleName, $moduleBase;var $stats = $wnd.__gwtStatsEvent ? function(a) {$wnd.__gwtStatsEvent(a)} : null;var $strongName = '1E2CB31439A7C1BCE101B96751551E2B';var $intern_0 = 2147483647, $intern_1 = {3:1, 18:1}, $intern_2 = {3:1, 11:1, 18:1}, $intern_3 = {3:1}, $intern_4 = 1000, $intern_5 = 65535, $intern_6 = 4194303, $intern_7 = 1048575, $intern_8 = 524288, $intern_9 = 4194304, $intern_10 = 17592186044416, $intern_11 = -9223372036854775808, $intern_12 = {422:1}, $intern_13 = 65536, $intern_14 = 131072, $intern_15 = 1048576, $intern_16 = 16777216, $intern_17 = 67108864, $intern_18 = {4:1}, $intern_19 = 1469, $intern_20 = 1493, $intern_21 = 1472, $intern_22 = 1495, $intern_23 = 1483, $intern_24 = 1023, $intern_25 = 1455, $intern_26 = {9:1}, $intern_27 = {68:1}, $intern_28 = 1458, $intern_29 = {43:1, 3:1}, $intern_30 = {30:1}, $intern_31 = 5.9604644775390625E-8, $intern_32 = {147:1}, $intern_33 = {3:1, 48:1, 91:1}, $intern_34 = {5:1, 3:1, 11:1, 18:1}, $intern_35 = 1.52587890625E-5, $intern_36 = 9.5367431640625E-7, $intern_37 = 2.220446049250313E-16, $intern_38 = {138:1}, $intern_39 = {90:1}, $intern_40 = {93:1, 55:1}, $intern_41 = {93:1, 103:1, 55:1}, $intern_42 = {3:1, 138:1}, $intern_43 = 15525485, $intern_44 = {3:1, 27:1, 12:1, 72:1}, $intern_45 = 1452;
+(function () {var $gwt_version = "2.7.0";var $wnd = window;var $doc = $wnd.document;var $moduleName, $moduleBase;var $stats = $wnd.__gwtStatsEvent ? function(a) {$wnd.__gwtStatsEvent(a)} : null;var $strongName = '0C13D2C542A46C8ABA264AD331488ED1';var $intern_0 = 2147483647, $intern_1 = {3:1, 18:1}, $intern_2 = {3:1, 11:1, 18:1}, $intern_3 = {3:1}, $intern_4 = 1000, $intern_5 = 65535, $intern_6 = 4194303, $intern_7 = 1048575, $intern_8 = 524288, $intern_9 = 4194304, $intern_10 = 17592186044416, $intern_11 = -9223372036854775808, $intern_12 = {422:1}, $intern_13 = 65536, $intern_14 = 131072, $intern_15 = 1048576, $intern_16 = 16777216, $intern_17 = 67108864, $intern_18 = {4:1}, $intern_19 = 1469, $intern_20 = 1493, $intern_21 = 1472, $intern_22 = 1495, $intern_23 = 1483, $intern_24 = 1023, $intern_25 = 1455, $intern_26 = {9:1}, $intern_27 = {68:1}, $intern_28 = 1458, $intern_29 = {43:1, 3:1}, $intern_30 = {30:1}, $intern_31 = 5.9604644775390625E-8, $intern_32 = {147:1}, $intern_33 = {3:1, 48:1, 91:1}, $intern_34 = {5:1, 3:1, 11:1, 18:1}, $intern_35 = 1.52587890625E-5, $intern_36 = 9.5367431640625E-7, $intern_37 = 2.220446049250313E-16, $intern_38 = {138:1}, $intern_39 = {90:1}, $intern_40 = {93:1, 55:1}, $intern_41 = {93:1, 103:1, 55:1}, $intern_42 = {3:1, 138:1}, $intern_43 = 15525485, $intern_44 = {3:1, 27:1, 12:1, 72:1}, $intern_45 = 1452;
 var _, initFnList_0, prototypesByTypeId_0 = {}, permutationId = -1;
 function im_actor_core_js_providers_Assets(){
   switch (permutationId) {
@@ -40309,7 +40309,7 @@ function $putLong(this$static, key, v){
 
 function $putString(this$static, key, v){
   key = 'prefs_' + key;
-  v == null?$removeItem(this$static.storage, key):$setItem(this$static.storage, 'prefs_' + key, v);
+  v == null?$removeItem(this$static.storage, key):$setItem(this$static.storage, key, v);
 }
 
 function JsPreferencesStorage(storage){
