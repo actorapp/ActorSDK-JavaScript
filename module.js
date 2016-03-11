c5036c797fbb928e8a517827e390dc01c888c76f
Nikita Gusakov <dev@nkt.me>
2016-03-11 15:57:32 +0300

    fix(module): update create method to the latest API

--- module.js
+++ module.js
@@ -2,8 +2,16 @@ actor = require('./actor.js')
 
 module.exports = {
   create: function(config) {
+    if (Array.isArray(config)) {
+      console.error('Deprecation notice: first argument should be an object with "endpoints" key.');
+      config = {
+        endpoints: config
+      };
+    }
+
     var client = new actor.ActorApp();
     client.init(config);
+
     return client;
   }
-}
+};
